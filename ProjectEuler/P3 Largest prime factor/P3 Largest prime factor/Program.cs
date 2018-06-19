767424a8c4a262cf16da8a0c2fb91e83ea3e004b
Maciej Bruski <m.globalbot@gmail.com>
2018-06-19 09:14:49 +0200

    ProjectEuler
    1-4 PE

---
+++ ProjectEuler/P3 Largest prime factor/P3 Largest prime factor/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// prime factors of 13195 are 5, 7, 13 and 29.
+// What is the largest prime factor of the number 600851475143 ?
+
+namespace P3_Largest_prime_factor
+{
+    class Program
+    {
+
+        static void Main(string[] args)    
+        {
+            var number = 600851475143;
+
+            for (var p = 1; p<=Math.Sqrt(number); p++)
+                if (number % p == 0)
+                {
+                    bool result = IsPrimeNumber(p);
+                    if (result == true) Console.WriteLine(p);
+                }
+
+
+           Console.ReadLine();
+        }
+
+        public static bool IsPrimeNumber(int factorOfNumber)
+        {
+            if (factorOfNumber <= 1)
+            {
+                return false; // because definition
+            }
+
+            var x = (int)Math.Sqrt(factorOfNumber);
+            for (var i = 2; i <= x; i++)
+            {
+                if (factorOfNumber % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+    }
+}
---
+++ ProjectEuler/P4 Largest palindrome product/P4 Largest palindrome product/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//A palindromic number reads the same both ways.
+//The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+//Find the largest palindrome made from the product of two 3-digit numbers.
+
+namespace P4_Largest_palindrome_product
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool result;
+            var a = 999;
+            var b = 999;
+            var x = 0;
+            var maxPali = 0;
+            while (b > 99)
+            {
+                x = a * b;
+                result = IsPalindrome(x);
+                if (result == true)
+                {
+                    if (x > maxPali)
+                    {
+                        maxPali = x;
+                        Console.WriteLine(a + " * " + b + " = " + x);
+
+                    }
+                    
+                }
+                a--;
+                if (a==100)
+                {
+                    b--;
+                    a = 999;
+                }
+                
+            }
+           
+            Console.ReadLine();
+
+
+        }
+
+        public static bool IsPalindrome(int number)
+        {
+            var charArray = number.ToString().ToCharArray();
+
+            for (var i = 0; i < charArray.Length; i++)
+            {
+                var isEqual = charArray[i] == charArray[charArray.Length - i - 1];
+                if (!isEqual)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
