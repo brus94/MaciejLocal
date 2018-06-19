47cd75be54ac15c1543a508a79b71f0a92065223
Maciej Bruski <m.globalbot@gmail.com>
2018-06-20 00:18:35 +0200

    p7
    p7

---
+++ ProjectEuler/P6 Sum square difference/P6 Sum square difference/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+The sum of the squares of the first ten natural numbers is,
+12 + 22 + ... + 102 = 385
+
+The square of the sum of the first ten natural numbers is,
+(1 + 2 + ... + 10)2 = 552 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+*/
+
+namespace P6_Sum_square_difference
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            long diff = 0;
+
+            var number = 100;
+
+            var sumOfSquares = SumOfSquares(number);
+            var squareOfSum = SquareOfSum(number);
+
+            diff = squareOfSum - sumOfSquares;
+
+            Console.WriteLine(diff);
+            Console.ReadLine();
+        }
+        public static long SumOfSquares(long number)
+        {
+            var sum = 0;
+            for (var i = 1; i <= number; i++)
+            {
+                sum = sum + (i * i);
+            }
+            return sum;
+        }
+
+        public static long SquareOfSum(long number)
+        {
+            var sum = 0;
+            for (var i = 1; i <= number; i++)
+            {
+                sum = sum + i;
+            }
+            sum = sum * sum;
+            return sum;
+        }
+
+    }
+}
---
+++ ProjectEuler/P7 10001st prime/P7 10001st prime/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+  By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+  What is the 10 001st prime number?
+ */
+
+namespace P7_10001st_prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var j = 0;
+            var i = 2;
+            while (j<10001)
+            {
+                bool result = IsPrimeNumber(i);
+                if (result == true) j++;
+                i++;
+            }
+            
+            Console.ReadLine();
+
+        }
+        public static bool IsPrimeNumber(int number)
+        {
+            if (number <= 1)
+            {
+                return false; 
+            }
+            
+            var x = (int)Math.Sqrt(number);
+            for (var i = 2; i <= x; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            Console.WriteLine(number);
+            return true;
+        }
+    }
+}
